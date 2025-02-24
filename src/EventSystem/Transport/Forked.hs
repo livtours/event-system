48226d3a363b0632fd5e9c1d985c2ccd2f4cc67b
Marco Perone <pasafama@gmail.com>
2025-02-24 11:56:45 +0100

    add Forked transport

---
+++ src/EventSystem/Transport/Forked.hs
@@ -0,0 +1,16 @@
+module EventSystem.Transport.Forked where
+
+import EventSystem.EventHandler (EventHandler (..))
+import EventSystem.Transport (Sender (..))
+
+import "base" Control.Concurrent (ThreadId)
+import "lifted-base" Control.Concurrent.Lifted (fork)
+import "monad-control" Control.Monad.Trans.Control (MonadBaseControl)
+
+-- | A `ForkedTransport` wraps an event handler, which is immediately executed, in another thread, whenever an event is received
+newtype ForkedTransport m event a = ForkedTransport (EventHandler m event a)
+
+instance (MonadBaseControl IO m) => Sender ForkedTransport m event () m ThreadId where
+  send :: ForkedTransport m event () -> event -> m ThreadId
+  send (ForkedTransport (EventHandler eventHandler)) event =
+    fork $ eventHandler event
